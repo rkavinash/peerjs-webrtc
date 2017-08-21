9251ca38c61b17507e8579bb818c803c6de30342
rkavinash <rkavinash@gmail.com>
2017-08-21 23:48:12 +0530

    UI re-factoring to make look good

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,22 @@ export class AppComponent implements OnInit {
   title = 'My Web RTC module';
 
   @ViewChild('myVideo') myVideo: any;
+  @ViewChild('theirVideo') theirVideo: any;
   peer: any;
   anotherPeersId: any;
-  myPeerId: any
+  myPeerId: any;
 
   constructor() {
   }
 
   ngOnInit() {
-      const video = this.myVideo.nativeElement;
+      const myVideo = this.myVideo.nativeElement;
+      const theirVideo = this.theirVideo.nativeElement;
       this.peer = new Peer({　host: 'ark-peerjs-server.herokuapp.com', secure: true, port: 443, key: 'peerjs', debug: 3})
 
       setTimeout(() => {
         this.myPeerId = this.peer.id;
-      }, 2000);
+      }, 3000);
 
       this.peer.on('connection', function(conn) {
         conn.on('data', function(data){
@@ -34,41 +36,46 @@ export class AppComponent implements OnInit {
 
       const n = <any> navigator;
       n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
+
+      n.getUserMedia({audio: true, video: true}, function(stream) {
+        myVideo.src = URL.createObjectURL(stream);
+      }, function(err) {
+          console.log('Failed to get local stream', err);
+      });
+
       this.peer.on('call', (call) => {
         n.getUserMedia({video: true, audio: true}, function(stream) {
           call.answer(stream); // Answer the call with an A/V stream.
           call.on('stream', (remoteStream) => {
-            // Show stream in some video/canvas element.
-            video.src = URL.createObjectURL(remoteStream);
-            video.play();
+            theirVideo.src = URL.createObjectURL(remoteStream);
           });
         }, function(err) {
-          console.log('Failed to get local stream', err);
+          console.log('Failed to get remote stream', err);
         });
       });
   }
 
-  connect() {
-    const conn = this.peer.connect(this.anotherPeersId);
-    conn.on('open', function(){
-      conn.send('hi............!');
-    });
-  }
-
-  videoConnect() {
-      const video = this.myVideo.nativeElement;
+  videoCall() {
+      const theirVideo = this.theirVideo.nativeElement;
       const n = <any> navigator;
       n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
 
       n.getUserMedia({video: true, audio: true}, (stream) => {
         const call = this.peer.call(this.anotherPeersId, stream);
         call.on('stream', (remoteStream) => {
-          // Show stream in some video/canvas element.
-          video.src = URL.createObjectURL(remoteStream);
-          video.play();
+          console.log('teiir video called');
+          theirVideo.src = URL.createObjectURL(remoteStream);
         });
       }, function(err) {
-        console.log('Failed to get local stream', err);
+        console.log('Failed to get remote stream', err);
+      });
+  }
+
+  endVideoCall() {
+    this.peer.on('destroy', () => {
+      console.log('destroy');
+      }, function(err) {
+        console.log('Failed to close remote stream', err);
       });
   }
 
